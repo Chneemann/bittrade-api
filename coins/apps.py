07b5c53f4206f2bb641a64c220d26f5f49c48e4a
Chneemann <andre-kempf@mailbox.org>
2025-05-25 12:47:42 +0200

    feat(coins): auto-import initial coins from JSON after migration in CoinsConfig using post_migrate signal

--- coins/apps.py
+++ coins/apps.py
@@ -1,5 +1,30 @@
+import os, json
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.db.utils import OperationalError
 
 class CoinsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coins'
+    
+    def ready(self):
+        from coins.models import Coin
+
+        def import_coins(sender, **kwargs):
+            try:
+                if Coin.objects.exists():
+                    return 
+                
+                path = os.path.join(os.path.dirname(__file__), 'initial_coins.json')
+                if os.path.exists(path):
+                    with open(path, 'r') as f:
+                        coins = json.load(f)
+                        for c in coins:
+                            Coin.objects.update_or_create(
+                                symbol=c['symbol'],
+                                defaults={'name': c['name']}
+                            )
+            except OperationalError:
+                pass
+
+        post_migrate.connect(import_coins, sender=self)
