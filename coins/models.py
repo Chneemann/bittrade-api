2d4658bc9733ee9b47b7f5d5418f79a936949ef3
Chneemann <andre-kempf@mailbox.org>
2025-05-25 14:06:08 +0200

    feat(coins): add automatic CoinHolding update and validation logic to CoinTransaction model

--- coins/models.py
+++ coins/models.py
@@ -1,6 +1,9 @@
 import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.db.models import Sum, F, DecimalField
+from decimal import Decimal
 
 User = get_user_model()
 
@@ -51,3 +54,62 @@ class CoinTransaction(models.Model):
     def __str__(self):
         type_display = dict(self.TRANSACTION_TYPES).get(self.transaction_type, self.transaction_type)
         return f"{self.user} {type_display} {self.amount} {self.coin.symbol}"
+
+    @staticmethod
+    def update_user_holding(user, coin):
+        transactions = CoinTransaction.objects.filter(user=user, coin=coin)
+
+        total_bought_amount = transactions.filter(transaction_type='buy').aggregate(
+            total=Sum('amount')
+        )['total'] or Decimal('0')
+
+        total_sold_amount = transactions.filter(transaction_type='sell').aggregate(
+            total=Sum('amount')
+        )['total'] or Decimal('0')
+
+        total_bought_cost = transactions.filter(transaction_type='buy').aggregate(
+            total=Sum(F('amount') * F('price_per_coin'), output_field=DecimalField())
+        )['total'] or Decimal('0')
+
+        total_amount = total_bought_amount - total_sold_amount
+
+        if total_amount > 0:
+            average_buy_price = total_bought_cost / total_bought_amount
+        else:
+            average_buy_price = Decimal('0')
+
+        holding, _ = CoinHolding.objects.get_or_create(user=user, coin=coin)
+        holding.amount = total_amount
+        holding.average_buy_price = average_buy_price
+        holding.save()
+
+    def clean(self):
+        if self.amount <= 0:
+            raise ValidationError({'amount': "Amount must be greater than zero."})
+        if self.price_per_coin <= 0:
+            raise ValidationError({'price_per_coin': "Price per coin must be greater than zero."})
+
+        if self.transaction_type == 'sell':
+            transactions = CoinTransaction.objects.filter(user=self.user, coin=self.coin)
+            if self.pk:
+                transactions = transactions.exclude(pk=self.pk)
+
+            total_bought = transactions.filter(transaction_type='buy').aggregate(Sum('amount'))['amount__sum'] or Decimal('0')
+            total_sold = transactions.filter(transaction_type='sell').aggregate(Sum('amount'))['amount__sum'] or Decimal('0')
+            current_amount = total_bought - total_sold
+
+            if self.amount > current_amount:
+                raise ValidationError({
+                    'amount': f"Not enough stock: You only have {current_amount:.8f} {self.coin.symbol}, "
+                              f"but would like to sell {self.amount:.8f}."
+                })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+        self.update_user_holding(self.user, self.coin)
+
+    def delete(self, *args, **kwargs):
+        user, coin = self.user, self.coin
+        super().delete(*args, **kwargs)
+        self.update_user_holding(user, coin)
