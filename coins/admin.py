13e93a40333c119ca0d40dbf7050bb704f91c552
Chneemann <andre-kempf@mailbox.org>
2025-05-25 13:14:05 +0200

    feat(coins): add is_active to Coin model and update Django admin to enhance control and restrict deletions

--- coins/admin.py
+++ coins/admin.py
@@ -3,15 +3,26 @@ from .models import Coin, CoinHolding, CoinTransaction
 
 @admin.register(Coin)
 class CoinAdmin(admin.ModelAdmin):
-    list_display = ['name', 'symbol']
+    list_display = ['name', 'symbol', 'is_active']
+    readonly_fields = ['name', 'symbol']
 
-
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
 @admin.register(CoinHolding)
 class CoinHoldingAdmin(admin.ModelAdmin):
     list_display = ['user', 'coin', 'amount', 'average_buy_price']
     readonly_fields = ['user', 'coin', 'amount', 'average_buy_price']
 
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 @admin.register(CoinTransaction)
 class CoinTransactionAdmin(admin.ModelAdmin):
     list_display = ['user', 'coin', 'transaction_type', 'amount', 'price_per_coin', 'created_at']
+    readonly_fields = ['created_at']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ['user', 'coin']
+        return self.readonly_fields 
--- coins/models.py
+++ coins/models.py
@@ -4,13 +4,14 @@ from django.conf import settings
 class Coin(models.Model):
     name = models.CharField(max_length=50)
     symbol = models.CharField(max_length=10, unique=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} ({self.symbol})"
-
+    
 class CoinHolding(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE, limit_choices_to={'is_active': True})
     amount = models.DecimalField(max_digits=20, decimal_places=8, default=0)
     average_buy_price = models.DecimalField(max_digits=20, decimal_places=8, default=0)
 
@@ -29,7 +30,7 @@ class CoinTransaction(models.Model):
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE, limit_choices_to={'is_active': True})
     transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
     amount = models.DecimalField(max_digits=20, decimal_places=8)
     price_per_coin = models.DecimalField(max_digits=20, decimal_places=8)
