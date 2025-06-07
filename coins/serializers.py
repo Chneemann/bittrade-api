5f242c98ba161282d5a9da0ed0bfb83a2338dbd0
Chneemann <andre-kempf@mailbox.org>
2025-06-07 18:15:39 +0200

    feat(coins): add serializers with nested coin info fields for transactions and holdings

--- coins/serializers.py
+++ coins/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
-from .models import CoinTransaction, CoinHolding
+from .models import CoinTransaction, CoinHolding, Coin
+
+class BaseCoinSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(read_only=True)
+    symbol = serializers.CharField(read_only=True)
+    slug = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Coin
+        fields = ['name', 'symbol', 'slug']
 
 class CoinTransactionSerializer(serializers.ModelSerializer):
-    slug = serializers.CharField(source='coin.slug', read_only=True)
+    coin = BaseCoinSerializer(read_only=True)
 
     class Meta:
         model = CoinTransaction
-        fields = ['id', 'slug', 'transaction_type', 'amount', 'price_per_coin', 'created_at']
+        fields = ['id', 'coin', 'transaction_type', 'amount', 'price_per_coin', 'created_at']
 
 class CoinHoldingSerializer(serializers.ModelSerializer):
-    slug = serializers.CharField(source='coin.slug', read_only=True)
+    coin = BaseCoinSerializer(read_only=True)
 
     class Meta:
         model = CoinHolding
-        fields = ['id', 'slug', 'amount', 'average_buy_price']
+        fields = ['id', 'coin', 'amount', 'average_buy_price']
