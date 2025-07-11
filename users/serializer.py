ef00f553fe64b56e3ab8adbbbd8352f566818b81
Chneemann <andre-kempf@mailbox.org>
2025-07-11 19:49:37 +0200

    feat(users): implement password change logic in UserUpdateSerializer

--- users/serializer.py
+++ users/serializer.py
@@ -6,6 +6,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -28,7 +29,7 @@ class LoginSerializer(serializers.Serializer):
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'password']
         extra_kwargs = {
             'username': {'required': False},
             'email': {'required': False},
@@ -39,7 +40,7 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         if User.objects.exclude(pk=user.pk).filter(email=value).exists():
             raise serializers.ValidationError("This email is already in use")
         return value
-
+    
     def validate_username(self, value):
         user = self.instance
         if User.objects.exclude(pk=user.pk).filter(username=value).exists():
@@ -52,19 +53,15 @@ class UserUpdateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Username must be at least 8 characters long")
 
         return value
+        
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long")
+        return value
     
     def is_email_changed(self):
         new_email = self.validated_data.get("email")
         return bool(new_email and new_email != self.instance.email)
-
-    def update(self, instance, validated_data):
-        new_email = validated_data.get('email')
-        if new_email and new_email != instance.email:
-            instance.unconfirmed_email = new_email
-            self.send_email_verification(instance)
-            validated_data.pop('email')
-
-        return super().update(instance, validated_data)
 
     def send_email_verification(self, user):
         token = default_token_generator.make_token(user)
@@ -77,3 +74,19 @@ class UserUpdateSerializer(serializers.ModelSerializer):
             from_email="noreply@your-domain.com",
             recipient_list=[user.unconfirmed_email],
         )
+
+    def update(self, instance, validated_data):
+        new_email = validated_data.get('email')
+        new_password = validated_data.pop('password', None)
+
+        if new_email and new_email != instance.email:
+            instance.unconfirmed_email = new_email
+            self.send_email_verification(instance)
+            validated_data.pop('email', None) 
+
+        if new_password:
+            instance.password = make_password(new_password)
+
+        instance = super().update(instance, validated_data)
+        instance.save()
+        return instance
