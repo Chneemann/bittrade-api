663bf85cbcc52602a875946169a4058fe1e96675
Chneemann <andre-kempf@mailbox.org>
2025-08-23 11:28:31 +0200

    feat: integrate Redis caching with Django configuration

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,21 @@ DATABASES = {
     }
 }
 
+# Redis
+
+CACHES = {
+    'default': {
+        'BACKEND': "django_redis.cache.RedisCache",
+        'LOCATION': "redis://127.0.0.1:6379/1",
+        'OPTIONS': {
+            'PASSWORD': 'foobared',
+            'CLIENT_CLASS': "django_redis.client.DefaultClient"
+            },
+        'KEY_PREFIX': "bittrade",
+        'TIMEOUT': 24 * 60 * 60,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
