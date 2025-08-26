a1356a4cf9503f74cdfc8e3407eb7b786166c252
Chneemann <andre-kempf@mailbox.org>
2025-08-26 07:58:31 +0200

    feat(caches): add CachedCoinListView API to retrieve cached active coins from Redis

--- caches/views.py
+++ caches/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from django.core.cache import cache
 
 from config.celery import app  
 from coins.models import Coin
@@ -21,3 +22,16 @@ class QueueCoinCacheView(APIView):
                 results.append(f"{coin.slug} failed: {str(e)}")
 
         return Response({"results": results}, status=status.HTTP_200_OK)
+
+class CachedCoinListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        coins = Coin.objects.filter(is_active=True)
+        data = {}
+        for coin in coins:
+            redis_key = f"coin:{coin.slug}"
+            cached = cache.get(redis_key)
+            if cached:
+                data[coin.slug] = cached 
+        return Response(data)
